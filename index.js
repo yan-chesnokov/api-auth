ccf822a30126aefaafc4ee01444e06c70d3f0264
Yan Chesnokov <kair.hot@gmail.com>
2020-11-29 19:56:39 +0400

    mongoose connect

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
 
+dotenv.config();
+
+//connect to DB
+mongoose.connect(process.env.DB_CONNECT || 'mongoose url',{useNewUrlParser:true},() => console.log('connected to DB'))
+//MiddLewara
+app.use(express.json());
 //import router
 const autchRoute = require('./router/autch')
-app.use('api/user',autchRoute)
+app.use('/api/user',autchRoute)
 
 app.listen(3000, () => console.log("Server Up and ranning 3000"))
