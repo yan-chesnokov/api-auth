ade9dcb120cfc64cdf70cd0560099906d49ae186
Yan Chesnokov <kair.hot@gmail.com>
2020-11-30 00:47:19 +0400

    jwt-authentication

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ mongoose.connect(process.env.DB_CONNECT || 'mongoose url',{useNewUrlParser:true}
 //MiddLewara
 app.use(express.json());
 //import router
-const autchRoute = require('./router/autch')
-app.use('/api/user',autchRoute)
+const autchRoute = require('./router/autch');
+app.use('/api/user',autchRoute);
+const postsRoute = require('./router/User');
+app.use('/api/posts',postsRoute);
 
-app.listen(3000, () => console.log("Server Up and ranning 3000"))
+app.listen(3000, () => console.log("Server Up and ranning 3000"));
--- router/verifyToken.js
+++ router/verifyToken.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-function autch (req,res,next) {
+module.exports = function (req,res,next) {
     const token = req.header('auth-token');
-    if(!token) return res.statun(401).send('Access Denied');
+    if(!token) return res.status(401).send('Access Denied');
 
     try{
         const verified = jwt.verify(token, process.env.TOKEN_SECRET || 'Supersecret');
         req.user = verified;
+        next();
     }catch(err){
         res.statun(400).send('Invalid Token');
     }
