8c52d7049711ab9beaa54bc7ef044e67317a4aa6
Yan Chesnokov <kair.hot@gmail.com>
2020-11-29 22:50:13 +0400

    validation

--- router/autch.js
+++ router/autch.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const { registerValidation } = require('./validation');
+
 
 router.post('/register', async ( req,res) => {
+//LETS VALIDATE THE DATA BEFORE WE A USER
+const { error } = registerValidation(req.body);
+if(error) return res.status(400).send(error.details[0].message);
+//CHECK USER DATABASE
+const emailExit = await User.findOne({email: req.body.email});
+if (emailExit) return res.status(400).send('Email alreadey exits');
+//CREATE USER
     const user = new User({
       name: req.body.name,
       email: req.body.email,
---
+++ router/validation.js
@@ -0,0 +1,17 @@
+//VALLIDATION
+const { date } = require('joi');
+const joi =require('joi');
+
+//REGISTER VALLIDATION
+const registerValidation = date => {
+const shema = joi.object({
+  name : joi.string().min(6).required(),
+  email: joi.string().min(6).required().email(),
+  password: joi.string().min(6).required()
+})
+//LETS VALIDATE THE DATA BEFORE WE A USER
+return shema.validate(date);
+};
+
+
+module.exports.registerValidation = registerValidation;
